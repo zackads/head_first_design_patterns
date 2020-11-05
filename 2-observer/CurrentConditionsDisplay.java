34beb55515db0f5acc9193345859b571980e9662
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-05 20:19:46 +0000

    Complete observer pattern exercise

--- 2-observer/CurrentConditionsDisplay.java
+++ 2-observer/CurrentConditionsDisplay.java
@@ -1,9 +1,37 @@
-public class CurrentConditionsDisplay implements Observer, DisplayElement {
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement { // Observer = from java.util.Observer
+    Observable observable;
+    private float temperature;
+    private float humidity;
+
+    public CurrentConditionsDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
+    }
+
+    public void update(Observable obs, Object arg) {
+        if (obs instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData)obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
+
+/*
+class OldCurrentConditionsDisplay implements Observer, DisplayElement { // Observer = the RYO version, from Observer.java
     private float temperature;
     private float humidity;
     private Subject weatherData;
 
-    public CurrentConditionsDisplay(Subject weatherData) {
+    public OldCurrentConditionsDisplay(Subject weatherData) {
         this.weatherData = weatherData;
         weatherData.registerObserver(this);
     }
@@ -18,3 +46,4 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement {
         System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
     }
 }
+*/
--- 2-observer/WeatherData.java
+++ 2-observer/WeatherData.java
@@ -1,12 +1,47 @@
-import java.util.ArrayList;
+import java.util.Observable;
+import java.util.ArrayList; // For OldWeatherData only
 
-public class WeatherData implements Subject {
+public class WeatherData extends Observable {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() { }
+
+    public void measurementsChanged() { 
+        setChanged();
+        notifyObservers(); // No object passed as argument, so this is the PULL model
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+}
+
+// Roll-your-own version of a Subject in the Observer pattern
+// Doesn't use the Java built-in Observable
+class OldWeatherData implements Subject {
     private ArrayList<Observer> observers;
     private float temperature;
     private float humidity;
     private float pressure;
 
-    public WeatherData() {
+    public OldWeatherData() {
         observers = new ArrayList<Observer>();
     }
 
