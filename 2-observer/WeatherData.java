34a7d470fef933db48b8c815f2dbe0848bd5831d
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-05 15:58:06 +0000

    Create WeatherData class

---
+++ 2-observer/WeatherData.java
@@ -0,0 +1,32 @@
+public class WeatherData implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) {
+            observers.remove(i);
+        }
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
