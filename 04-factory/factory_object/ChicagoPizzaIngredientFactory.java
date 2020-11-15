913cea46b6a8073e6548e20149e224b8e0c82300
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-15 15:43:44 +0000

    Define ChicagoIngredientFactory

---
+++ 04-factory/factory_object/ChicagoPizzaIngredientFactory.java
@@ -0,0 +1,25 @@
+public class ChicagoIngredientFactory implements PizzaIngredientFactory {
+    public Dough createDough() {
+        return new ThickCrustDough();
+    };
+
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    };
+
+    public Cheese createCheese() {
+        return new MozzarellaCheese();
+    };
+
+    public Veggies[] createVieggies() {
+        Veggies veggies[] = { new Spinach(); new BlackOlives(); new EggPlant(); }
+    };
+
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    };
+
+    public Clams createClam() {
+        return new FrozenClam();
+    };
+}
