b29ffee4ac08c1a4fe187dd82dead75884e14100
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-15 09:44:50 +0000

    Name folders with 2 x number digits (1 -> 01)

---
+++ 04-factory/simple_factory/CheesePizza.java
@@ -0,0 +1,17 @@
+public class CheesePizza extends Pizza {
+    public void prepare() {
+        System.out.println("Preparing cheese pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting cheese pizza in oven to bake...");
+    }
+
+    public void prepare() {
+        System.out.println("Pulling out of the oven and cutting the cheese pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting the cheese pizza in a box...");
+    }
+} 
