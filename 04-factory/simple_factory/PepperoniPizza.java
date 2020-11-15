5e77ceb31b49ad36a37e0d2869d21bbf4eef6982
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-15 09:46:24 +0000

    Define concrete pizza classes

---
+++ 04-factory/simple_factory/PepperoniPizza.java
@@ -0,0 +1,17 @@
+public class PepperoniPizza extends Pizza {
+    public void prepare() {
+        System.out.println("Preparing Pepperoni pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting Pepperoni pizza in oven to bake...");
+    }
+
+    public void prepare() {
+        System.out.println("Pulling out of the oven and cutting the Pepperoni pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting the Pepperoni pizza in a box...");
+    }
+} 
---
+++ 04-factory/simple_factory/VeggiePizza.java
@@ -0,0 +1,17 @@
+public class VeggiePizza extends Pizza {
+    public void prepare() {
+        System.out.println("Preparing Veggie pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting Veggie pizza in oven to bake...");
+    }
+
+    public void prepare() {
+        System.out.println("Pulling out of the oven and cutting the Veggie pizza...");
+    }
+
+    public void prepare() {
+        System.out.println("Putting the Veggie pizza in a box...");
+    }
+} 
