65a7e750e426b17d2935970d9487b2dc4d9f46b0
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-05 20:25:58 +0000

    Add state to CeilingFan

--- 06-command/CeilingFan.java
+++ 06-command/CeilingFan.java
@@ -1,32 +1,37 @@
 public class CeilingFan {
-    String speed;
-    String room;
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
+    int speed;
+    String location;
 
-    public CeilingFan(String room) {
-        this.room = room;
+    public CeilingFan(String location) {
+        this.location = location;
+        speed = OFF;
     }
 
     public void high() {
-        this.speed = "high";
-        System.out.println(this.room + " ceiling fan is on high");
+        speed = HIGH;
+        System.out.println(this.location + " ceiling fan is on high");
     }
 
     public void medium() {
-        this.speed = "medium";
-        System.out.println(this.room + " ceiling fan is on medium");
+        speed = MEDIUM;
+        System.out.println(this.location + " ceiling fan is on medium");
     }
 
     public void low() {
-        this.speed = "low";
-        System.out.println(this.room + " ceiling fan is on low");
+        speed = LOW;
+        System.out.println(this.location + " ceiling fan is on low");
     }
 
     public void off() {
-        this.speed = "off";
-        System.out.println(this.room + " ceiling fan is off");
+        speed = OFF;
+        System.out.println(this.location + " ceiling fan is off");
     }
 
     public void getSpeed() {
-        System.out.println(this.speed);
+        return speed;
     }
 }
