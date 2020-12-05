5337245fee93f7ed842aaaf5eb8c6c8476fbbb74
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-05 17:15:37 +0000

    Define SimpleRemoteControl client class

---
+++ 06-command/SimpleRemoteControl.java
@@ -0,0 +1,12 @@
+public class SimpleRemoteControl {
+    Command slot;
+    public SimpleRemoteControl() {}
+
+    public void setCommand(Command command) {
+        slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
