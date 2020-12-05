21938cf2e63ca3684c300a22bc1c9aa5f8c15460
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-05 22:08:34 +0000

    Complete Command Pattern chapter

--- 06-command/RemoteLoader.java
+++ 06-command/RemoteLoader.java
@@ -8,24 +8,14 @@ public class RemoteLoader {
         GarageDoor garageDoor = new GarageDoor("");
         Stereo stereo = new Stereo("Living Room");
 
-        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-
-        CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
-
-        GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
-        GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
-
-        StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
-        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
-
-        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
-        remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
-        remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+        remoteControl.setCommand(0, livingRoomLight::on, livingRoomLight::off);
+        remoteControl.setCommand(1, kitchenLight::on, kitchenLight::off);
+        remoteControl.setCommand(2, ceilingFan::medium, ceilingFan::off);
+        Command stereoOnWithCD = () -> {
+            stereo.on(); stereo.setCd(); stereo.setVolume(11);
+        };
+        remoteControl.setCommand(3, stereoOnWithCD, stereo::off);
+        remoteControl.setCommand(4, garageDoor::up, garageDoor::down);
 
         System.out.println(remoteControl);
 
